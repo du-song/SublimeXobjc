d0d229cc014796be688051934710b7b8841ac6c3
Du Song <freewizard@gmail.com>
2012-01-10 14:52:08 +0800

    fix async file load

--- XobjcCommand.py
+++ XobjcCommand.py
@@ -6,17 +6,14 @@ import xobjc
 
 class XobjcCommandQueue(sublime_plugin.EventListener):
 	pending = {}
-	command = None
-	def set_command(self, command):
-		self.command = command
 
 	def on_load(self, view):
-		print self.command
 		buffer_id = self.pending.get(view.buffer_id())
 		if buffer_id:
 			(v1, v2) = self.pending[view.buffer_id()]
 			del self.pending[view.buffer_id()]
-			self.command._run(v1, v2)
+			self.run(v1, v2)
+			v1.window().focus_view(v1)
 
 	def on_close(self, view):
 		buffer_id = self.pending.get(view.buffer_id())
@@ -24,32 +21,7 @@ class XobjcCommandQueue(sublime_plugin.EventListener):
 			del self.pending[view.buffer_id()]
 		#FIXME may be buggy if close view1 too soon
 
-class XobjcCommand(sublime_plugin.TextCommand):
-	queue = None
-	def is_enabled(self):
-		f = self.view.file_name();
-		return f and f.endswith(("m", "h")); # for .h .m .mm
-
-	def run(self, edit):
-		v1 = self.view
-		f1 = v1.file_name()
-		if f1.endswith("h"):
-			f2 = os.path.splitext(f1)[0] + ".mm"
-			if not os.path.isfile(f2): f2 = os.path.splitext(f1)[0] + ".m"
-		else:
-			f2 = os.path.splitext(f1)[0] + ".h"
-		v2 = v1.window().open_file(f2) #, sublime.TRANSIENT
-		v2.window().focus_view(v1)
-		if v2.is_loading():
-			if not self.queue:
-				self.queue = XobjcCommandQueue()
-				self.queue.set_command(self)
-			print self.queue.command
-			self.queue.pending[v2.buffer_id()] = (v1, v2)
-		else:
-			self._run(v1, v2);
-
-	def _run(self, view_h, view_m):
+	def run(self, view_h, view_m):
 		settings = view_h.settings()
 		xobjc.BACKUP_FOLDER = settings.get("xobjc_backup_folder") or os.path.expandvars('${HOME}/work/_build/__xobjc_backup') #not in use
 		xobjc.BOOL_WITH_IS_GETTER = settings.get("xobjc_bool_getter_with_is") or False
@@ -81,4 +53,26 @@ class XobjcCommand(sublime_plugin.TextCommand):
 			sublime.status_message("Xobjc: code has been updated")
 		else:
 			sublime.status_message("Xobjc: no updated needed")
+
+
+class XobjcCommand(sublime_plugin.TextCommand):
+	queue = XobjcCommandQueue()
+	def is_enabled(self):
+		f = self.view.file_name();
+		return f and f.endswith(("m", "h")); # for .h .m .mm
+
+	def run(self, edit):
+		v1 = self.view
+		f1 = v1.file_name()
+		if f1.endswith("h"):
+			f2 = os.path.splitext(f1)[0] + ".mm"
+			if not os.path.isfile(f2): f2 = os.path.splitext(f1)[0] + ".m"
+		else:
+			f2 = os.path.splitext(f1)[0] + ".h"
+		v2 = v1.window().open_file(f2, sublime.TRANSIENT)
+		if v2.is_loading():
+			self.queue.pending[v2.buffer_id()] = (v1, v2)
+		else:
+			self.queue.run(v1, v2);
+			v2.window().focus_view(v1)
 
